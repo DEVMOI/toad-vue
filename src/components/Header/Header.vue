4960b323beff3f649ea6bcda1b57ca4371f673d7
Moikapy <warrenckhan@gmail.com>
2018-10-08 10:55:28 -0400

    update header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,13 +1,22 @@
 <template>
-    $END$
+    <div class="toad-header container fnt-monospace">
+        <h2 class="display-4">{{title}}</h2>
+        <p>{{subtitle}}</p>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Header"
+        name: 'Header',
+        props: {
+            title: String,
+            subtitle: String,
+        }
     }
 </script>
-
-<style scoped>
-
+<style>
+    .toad-header{
+        /*CSS_HERE*/
+        /*COMING SOON*/
+    }
 </style>
