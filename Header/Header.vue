cfa2692b97332c7d6b93d56edacd1cf5c9f326ca
moikapy <warrenckhan@gmail.com>
2018-10-28 03:03:41 -0400

    simplified repo
    cleaned files for nodegg repo, added dynamic classes for header

---
+++ Header/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div :class="headerContainerStyle" class="bg-default toad-header">
+        <h2 :class='titleStyle' class="display-4">{{title}}</h2>
+        <p :class='subTitleStyle'>{{subtitle}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Header',
+        props: {
+            title: String,
+            subtitle: String,
+            headerContainerStyle: String,
+            titleStyle: String,
+            subTitleStyle: String
+
+        }
+    }
+</script>
+<style>
+    .toad-header{
+        width:100%
+    }
+</style>
